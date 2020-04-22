4d848ec2df4303bf54669a99d14de50938b2cd45
bibeezi <44236242+bibeezi@users.noreply.github.com>
2020-04-22 17:58:38 +0100

    Fixed variables

--- java/src/ie/tudublin/Gantt.java
+++ java/src/ie/tudublin/Gantt.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import processing.core.PApplet;
 import processing.data.Table;
 import processing.data.TableRow;
-import processing.event.MouseEvent;
 
 public class Gantt extends PApplet
 {	
@@ -13,21 +12,19 @@ public class Gantt extends PApplet
 	ArrayList<Task> tasks = new ArrayList<Task>();
 
 	// displayTask method variables
-	float taskX;
-	float taskY;
+	float taskX, taskY;
 	int taskSpace;
 
-	float textX; 
-	float textY;
+	float textX, textY, textXEnd;
 	int textSpace;
 
-	float rectX;
-	float rectY;
 	int rectH;
-	float colour;
-	float cOffset;
+	float rectOffset;
 
-	float hitTop;
+	float colour;
+	float colOffset;
+
+	float hitboxTop;
 	float hitbox;
 
 	int selectedL;
@@ -36,7 +33,7 @@ public class Gantt extends PApplet
 	public void settings()
 	{
 		size(800, 600);
-
+		
 		taskX = width * 0.05f;
 		taskY = height * 0.15f;
 		taskSpace = 40;
@@ -45,11 +42,16 @@ public class Gantt extends PApplet
 		textY = height * 0.08f;
 		textSpace = 21;
 
+		textXEnd = textX + (29 * textSpace);
+
 		rectH = 35;
+		rectOffset = rectH / 2;
+
 		colour = 0;
-		cOffset = 255 / 9;
+		colOffset = 255 / 9;
 
 		hitbox = 10;
+		hitboxTop = 0;
 
 		selectedL = -1;
 		selectedR = -1;
@@ -82,24 +84,21 @@ public class Gantt extends PApplet
 		// Task names
 		for(Task task : tasks)
 		{
-			textAlign(LEFT, CENTER);
 			fill(0, 0, 255);
+
+			textAlign(LEFT, CENTER);
 			text(task.getTask(), taskX, taskY);
+
 			taskY += taskSpace;
 		}
 
 		// Vertical lines and numbers
 		for(int count = 1; count < 31; count++)
 		{
-			if(count % 2 == 0)
-			{
-				stroke(255);
-			}
-			else
-			{
-				stroke(100);
-			}
+			// Alternates white or grey lines
+			colour = (count % 2 == 0) ? 255 : 100;
 
+			stroke(colour);
 			line(textX, textY, textX, height - textY);
 			
 			textAlign(CENTER, CENTER);
@@ -111,55 +110,58 @@ public class Gantt extends PApplet
 		// reset necessary variables
 		taskY = height * 0.15f;
 		textX = taskX * 3.5f; 
+		colour = 0;
 
 		// Rectangles
 		for(Task task : tasks)
 		{
 			fill(colour, 255, 255);
 			noStroke();
-			rect(map(task.getStart(), 1, 30, textX, textX + (29 * textSpace)),
-				 taskY - (rectH / 2),
+
+			rect(map(task.getStart(), 1, 30, textX, textXEnd),
+				 taskY - rectOffset,
 				 (task.getEnd() - task.getStart()) * textSpace,
 				 rectH,
 				 5);
 
 			taskY += taskSpace;
-			colour += cOffset;
+			colour += colOffset;
 		}
 
+		// reset necessary variables
 		taskY = height * 0.15f;
 		colour = 0;
 	}
 	
 	public void mousePressed()
 	{	
+		// Reset selected box
 		selectedL = -1;
 		selectedR = -1;
 
-		hitTop = height * 0.15f;
+		hitboxTop = height * 0.15f;
 
 		for(int i = 0; i < tasks.size(); i++)
         {
-			
 			Task task = tasks.get(i);
 
-			float left = map(task.getStart(), 1, 30, textX, textX + (29 * textSpace));
-			float right = map(task.getEnd(), 1, 30, textX, textX + (29 * textSpace));
+			float left = map(task.getStart(), 1, 30, textX, textXEnd);
+			float right = map(task.getEnd(), 1, 30, textX, textXEnd);
 
             if(mouseX > left && mouseX < left + hitbox && 
-               mouseY > hitTop - (rectH / 2) && mouseY < hitTop + (rectH / 2))
+               mouseY > hitboxTop - rectOffset && mouseY < hitboxTop + rectOffset)
             {
-				println("Mouse Pressed L");
+				//println("Mouse Pressed L");
 				selectedL = i;
 			}
 			else if(mouseX > right - hitbox && mouseX < right && 
-			mouseY > hitTop - (rectH / 2) && mouseY < hitTop + (rectH / 2))
+			mouseY > hitboxTop - rectOffset && mouseY < hitboxTop + rectOffset)
 			{
-				println("Mouse Pressed R");
+				//println("Mouse Pressed R");
 				selectedR = i;
 			}
 			
-			hitTop += taskSpace;
+			hitboxTop += taskSpace;
         }
 	}
 
@@ -167,35 +169,41 @@ public class Gantt extends PApplet
 	{
 		int size;
 
+		// Selected left of the box
 		if(selectedL != -1)
 		{
 			Task task = tasks.get(selectedL);
 
-			float left = map(task.getStart(), 1, 30, textX, textX + (29 * textSpace));
+			float left = map(task.getStart(), 1, 30, textX, textXEnd);
 
+			// Moving to the left
 			if(mouseX < left && mouseX > textX)
 			{
 				size = task.getStart() - 1;
 				task.setStart(size);
 			}
+			// Moving to the right
 			else if(task.getEnd() - task.getStart() != 1 && mouseX > textX && mouseX > left + textSpace)
 			{
 				size = task.getStart() + 1;
 				task.setStart(size);
 			}
 		}
+		// Selected right of the box
 		else if(selectedR != -1)
 		{
 			Task task = tasks.get(selectedR);
 
-			float right = map(task.getEnd(), 1, 30, textX, textX + (29 * textSpace));
+			float right = map(task.getEnd(), 1, 30, textX, textXEnd);
 
-			if(mouseX > right && mouseX < textX + (29 * textSpace))
+			// Moving to the left
+			if(mouseX > right && mouseX < textXEnd)
 			{
 				size = task.getEnd() + 1;
 				task.setEnd(size);
 			}
-			else if(task.getEnd() - task.getStart() != 1 && mouseX < textX + (29 * textSpace) && mouseX < right - textSpace)
+			// Moving to the right
+			else if(task.getEnd() - task.getStart() != 1 && mouseX < right - textSpace && mouseX < textXEnd)
 			{
 				size = task.getEnd() - 1;
 				task.setEnd(size);
@@ -211,12 +219,12 @@ public class Gantt extends PApplet
 		printTasks();
 
 		colorMode(HSB);
-
 	}
 	
 	public void draw()
 	{
 		background(0);
+
 		displayTasks();
 	}
 }
